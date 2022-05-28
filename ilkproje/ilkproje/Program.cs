c7f705d895568085a725fbfda741dc2ac2016a4b
erayandic0 <106441824+erayandic0@users.noreply.github.com>
2022-05-28 19:07:32 +0300

    ilk proje

---
+++ ilkproje/ilkproje/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ilkproje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
